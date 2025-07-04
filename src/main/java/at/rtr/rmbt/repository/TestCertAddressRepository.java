3b1fd5b6e82b7cd31917fa86e51a88ad8b2e944d
David Furmanek <furmanekd@gmail.com>
2025-07-04 15:33:23 +0200

    Fix TestCertAddressRepository to use UUID as the primary key

--- src/main/java/at/rtr/rmbt/repository/TestCertAddressRepository.java
+++ src/main/java/at/rtr/rmbt/repository/TestCertAddressRepository.java
@@ -3,7 +3,9 @@ package at.rtr.rmbt.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.UUID;
+
 import at.rtr.rmbt.model.TestCertAddress;
 
-public interface TestCertAddressRepository extends JpaRepository<TestCertAddress, Long> {
+public interface TestCertAddressRepository extends JpaRepository<TestCertAddress, UUID> {
 }
