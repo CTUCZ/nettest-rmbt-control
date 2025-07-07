edec5268aeb8f9c7a47af61509f692eedd5a250f
David Furmanek <furmanekd@gmail.com>
2025-07-07 16:26:45 +0200

    Fix nullable userCertMode parameter in test result request

--- src/main/java/at/rtr/rmbt/service/impl/ResultServiceImpl.java
+++ src/main/java/at/rtr/rmbt/service/impl/ResultServiceImpl.java
@@ -91,7 +91,10 @@ public class ResultServiceImpl implements ResultService {
     }
 
     private void processCertMode(ResultRequest resultRequest, Test test) {
-        if (resultRequest.getUserCertMode() && Objects.nonNull(test.getLoopModeSettings())) {
+        if (Objects.nonNull(resultRequest.getUserCertMode()) &&
+            resultRequest.getUserCertMode() &&
+            Objects.nonNull(test.getLoopModeSettings())) {
+
             log.info("UserCertMode is true for test result uuid: {}", test.getUuid());
             test.getLoopModeSettings().setCertMode(resultRequest.getUserCertMode());
             loopModeSettingsRepository.save(test.getLoopModeSettings());
